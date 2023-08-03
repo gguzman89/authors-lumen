7208f491ff8324f02dd201525e9ec93442c0a07f
Gabriel Guzman <ggtpi0gabriel@gmail.com>
2023-08-02 22:25:43 -0300

    Middleware: Enable CORS in Lumen
    * add file
    * config app.php

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+use Closure;
+
+
+class CorsMiddleware
+{
+    public function handle($request, Closure $next) 
+    {
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => '*'
+        ];
+
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
+        return $response;
+    }
+}
